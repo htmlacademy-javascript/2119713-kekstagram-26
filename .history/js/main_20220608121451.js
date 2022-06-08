565eaa3a8a2fc2b79957df9b4754173943b3f20e
o.kartasheva <o.kartasheva@qiwi.com>
2022-06-08 15:54:58 +0300

    removed .history

--- .history/js/main_20220608121451.js
+++
@@ -1,5 +0,0 @@
-function randomInteger(min, max) {
-  //случайное целое от min до (max+1), так как в Math.random max не включительно
-  let rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
-}
